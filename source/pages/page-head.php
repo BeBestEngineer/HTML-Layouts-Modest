11cf50f2bbba61d7f71d0522dd8050e46bd90f4c
Alexandr Ja <mr-spinn@outlook.com>
2017-02-20 12:11:01 +0600

    add owl carousel styles to link tag

--- source/pages/page-head.php
+++ source/pages/page-head.php
@@ -7,7 +7,7 @@
     <!--third-components scripts-->
     <link rel="stylesheet" href="components/components-libs/normalize-css-5.0.0/normalize-5.0.0.css">
     <link rel="stylesheet" href="components/components-frameworks/bootstrap-3.3.7/css/bootstrap-3.3.7.css">
-    <link rel="stylesheet" href="components/components-libs/owl-carousel-1.3.3/owl-carousel/owl.carousel.css">    
+    <link rel="stylesheet" href="components/components-libs/owl-carousel-1.3.3/owl-carousel/owl.carousel.css">
     <!--layout's styles-->
     <link rel="stylesheet" href="components/components-styles/collected-styles/css/layout.css">
   </head>
